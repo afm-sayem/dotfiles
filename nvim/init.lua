001966d8aae4bc60f065d89d5a07e387e9497f5a
Sayem <sayem.afm@dtone.com>
2024-06-13 21:48:51 +0800

    chore: update packages

--- nvim/init.lua
+++ nvim/init.lua
@@ -21,20 +21,20 @@ Website: https://github.com/brainfucksec/neovim-lua
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- lazy requires leader to be setup now
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
-require('lazy').setup('plugins')
-require('settings')
-require('keymaps')
+require("lazy").setup("plugins")
+require("settings")
+require("keymaps")
